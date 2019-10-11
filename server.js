93fb269632710e27f8461aaca7630002ef14f744
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-10-10 21:16:15 -0500

    Server with express

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+
+app.post("/users",function(req, res) { 
+    res.status(200).json({
+        result: "succes",
+        status: 200
+    });
+});
+
+app.listen(3000, () => { 
+    console.log("server is running on port 3000");
+});
+
