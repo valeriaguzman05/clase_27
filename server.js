f557f39ee6292b22b8828e0d3647f603a6bc63fa
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-10-10 21:25:18 -0500

    cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 
+app.use(cors());
 app.post("/users",function(req, res) { 
     res.status(200).json({
         result: "success",
