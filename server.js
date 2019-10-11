14358478dbd7ac701b052a091e0c44693ed7f1f1
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-10-10 21:28:35 -0500

    body-parser

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 
+
 app.use(cors());
+app.use(bodyParser.json());
 app.post("/users",function(req, res) { 
     res.status(200).json({
         result: "success",
